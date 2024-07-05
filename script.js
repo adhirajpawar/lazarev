522f36ac14c0476bcf586cf52ea829a414bd7782
Adhiraj Pawar <adhirajpawar3@gmail.com>
2024-07-05 09:33:10 +0530

    messagee

---
+++ script.js
@@ -0,0 +1,39 @@
+function navAnimation() {
+var nav = document.querySelector("nav")
+
+nav.addEventListener("mouseenter",function(){
+    let tl = gsap.timeline()
+
+    tl.to("#navv-bottom",{
+        height:"21vh"
+    })
+    tl.to(".nav-part2 h5",{
+        display:"block"
+    })
+    tl.from(".nav-part2 h5 span",{
+        y:0,
+        stagger:{
+            amount:0.6
+        }
+    })
+})
+
+nav.addEventListener("mouseleave",function(){
+    let tl = gsap.timeline()
+    tl.to(".nav-part2 h5 span",{
+        y:25,
+        stagger:{
+            amount:0.2
+        }
+    })
+    tl.to(".nav-part2 h5",{
+        display:"none",
+        duration:0.1
+    })
+    tl.to("#navv-bottom",{
+        height:0,
+        duration:0.2
+    })
+})
+
+}
